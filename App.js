17fb5a7aea8b33743921931fda50de46fa8c9f17
Annylory LR <annyloryrosa@gmail.com>
2026-05-25 22:53:29 -0300

    fonts setting

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+import { getFontFamily } from './assets/fonts/helper';
+
+const App = () => {
+  
+  return (
+    <SafeAreaView>
+      <Text style={{fontSize:50, fontFamily: getFontFamily('Inter', '500') }}>Social Media App </Text>
+    </SafeAreaView>
+  
+  );
+}
+
+export default App;
